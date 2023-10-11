c8a12a18c86d4a55d69daca5fb06c0edc63a157a
LucasRachor <131928483+LucasRachor@users.noreply.github.com>
2023-10-11 18:58:21 -0400

    Update impar_par.js

--- impar_par.js
+++ impar_par.js
@@ -1,9 +1,15 @@
 function verificarNum(num1) {
-    if (num1 % 2 === 0) {
+
+    if (num1 == undefined) {
+        console.log('você precisa passar uma idade!')
+    } else if (typeof num1 !== 'number') {
+        console.log('Somente numeros permitidos!')
+    }
+    else if (num1 % 2 === 0) {
         console.log('par')
     } else {
         console.log('impar')
     }
 }
 
-verificarNum(129)
+verificarNum()
